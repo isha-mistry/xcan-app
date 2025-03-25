6a766e6d9996300fa84b5086d4be962c1e90d6f7
Hiralvala <valahiral563@gmail.com>
2025-03-25 17:07:00 +0530

    Added about information for let's grow dao.

--- src/components/ComponentUtils/IndividualDaoHeader.tsx
+++ src/components/ComponentUtils/IndividualDaoHeader.tsx
@@ -3,11 +3,13 @@ import ConnectWalletWithENS from "../ConnectWallet/ConnectWalletWithENS";
 import { usePathname, useRouter } from "next/navigation";
 import { dao_details } from "@/config/daoDetails";
 import RewardButton from "../ClaimReward/RewardButton";
+import { daoConfigs } from "@/config/daos";
 
 function IndividualDaoHeader() {
   const path = usePathname();
   const router = useRouter();
-  const dao_name = path.split("/").filter(Boolean)[0] || "";
+  const url_dao_name = path.split("/").filter(Boolean)[0] || "";
+  const dao_name = daoConfigs[url_dao_name].name;
 
   const logoMapping: any = {};
   const options: any[] = [];
@@ -27,11 +29,11 @@ function IndividualDaoHeader() {
     image: selectedLogo,
   });
 
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null); 
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const handleMouseEnter = () => {
     if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);  //clear any existing timeout
+      clearTimeout(timeoutRef.current); //clear any existing timeout
       timeoutRef.current = null;
     }
     setIsOpen(true);
@@ -66,7 +68,9 @@ function IndividualDaoHeader() {
       >
         <div>
           <div className="capitalize text-xl sm:text-2xl md:text-3xl lg:text-4xl text-blue-shade-100 bg-white-200 outline-none cursor-pointer flex items-center justify-between transition duration-500">
-            <div className="mr-2 sm:mr-3 md:mr-4 lg:mr-5 flex items-center truncate">{selectedOption.label}</div>
+            <div className="mr-2 sm:mr-3 md:mr-4 lg:mr-5 flex items-center truncate">
+              {selectedOption.label}
+            </div>
             <svg
               className={`w-3 h-3 sm:w-3.5 sm:h-3.5 md:w-4 md:h-4 mr-1 sm:mr-1.5 md:mr-2 flex-shrink-0 ${
                 isOpen
@@ -102,7 +106,7 @@ function IndividualDaoHeader() {
                     }`}
                     onClick={() => selectOption(option)}
                   >
-                    {option.value}
+                    {option.label}
                   </div>
                   {index !== options.length - 1 && <hr />}
                 </div>
@@ -113,9 +117,9 @@ function IndividualDaoHeader() {
       </div>
 
       <div className="flex gap-1 xs:gap-2 items-center">
-              <RewardButton />
-              {/* <ConnectWalletWithENS /> */}
-            </div>
+        <RewardButton />
+        {/* <ConnectWalletWithENS /> */}
+      </div>
     </div>
   );
 }
--- src/config/daos/letsGrowDAO.ts
+++ src/config/daos/letsGrowDAO.ts
@@ -2,21 +2,21 @@ import letsGrow_logo from "@/assets/images/daos/letsGrow.jpg";
 
 export const letsgrowdao = {
   title: "Let's Grow DAO",
-  dao_name: "letsgrowdao",
+  dao_name: "Let's Grow DAO",
   chain_name: "Optimism",
   description:
-    "Let's Grow DAO is the heart of the Optimism network, an innovative layer 2 solution for faster, cheaper transactions on Ethereum. Think of it as a community-driven engine, where token holders govern upgrades, fees, and the overall direction of the Optimism ecosystem. With a focus on scaling Ethereum effectively and sustainably, Optimism Collective is building a brighter future for blockchain technology.",
+    "Let's Grow DAO is a decentralized autonomous organization (DAO) dedicated to fostering the growth and unity of the Regen Movement through the development and support of public goods. It aims to connect individuals, projects, and resources within the regenerative ecosystem, leveraging Web3 principles to create accessible tools, knowledge, and funding mechanisms that empower the adoption of regenerative practices and contribute to a more sustainable and equitable future for all.",
   contract_address: "",
   number_of_delegates: "21",
-  token_name: "OP",
+  token_name: "LGD",
   logo: letsGrow_logo,
   links: {
-    forum: "https://gov.optimism.io/",
-    website: "https://www.optimism.io/",
-    docs: "https://community.optimism.io/",
-    block_explorer: "https://optimistic.etherscan.io/",
-    twitter_profile: "https://twitter.com/Optimism",
-    governance_twitter_profile: "https://twitter.com/OptimismGov",
+    // forum: "https://gov.optimism.io/",
+    website: "https://www.letsgrow.network/",
+    docs: "https://garden.letsgrow.network/page-8020229466123785",
+    // block_explorer: "https://optimistic.etherscan.io/",
+    twitter_profile: "https://twitter.com/LetsGrowDAO",
+    // governance_twitter_profile: "https://twitter.com/OptimismGov",
   },
   api_links: {
     subgraph: {
