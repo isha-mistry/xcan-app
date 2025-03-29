25d33c9c24bfd050745d39490b6c622b14b82182
Hiralvala <valahiral563@gmail.com>
2025-03-29 14:03:53 +0530

    Fixed redirection issue in Authguard component.

--- src/components/ComponentUtils/AuthGuard.tsx
+++ src/components/ComponentUtils/AuthGuard.tsx
@@ -15,20 +15,31 @@ const AuthGuard: React.FC<AuthGuardProps> = ({ children }) => {
   const pathname = usePathname();
   const { authenticated, ready } = usePrivy();
   const [isLoading, setIsLoading] = useState(true);
+  const [isChecking, setIsChecking] = useState(true);
 
   useEffect(() => {
     if (ready) {
-      const isLoggedIn = isConnected || authenticated;
+      // Delay authentication check if not initially connected
+      if (isChecking && !isConnected) {
+        const timeoutId = setTimeout(() => {
+          setIsLoading(false);
+          setIsChecking(false);
+        }, 2000); // 2 seconds timeout
+
+        return () => clearTimeout(timeoutId); // Cleanup timeout on unmount or re-run
+      }
+      const isLoggedIn = isConnected && authenticated;
 
       if (!isLoggedIn && pathname !== "/login") {
         router.push("/login");
       }
 
       setIsLoading(false);
+      setIsChecking(false);
     }
-  }, [isConnected, authenticated, router, pathname, ready]);
+  }, [isConnected, authenticated, router, pathname, ready, isChecking]);
 
-  if (isLoading) {
+  if (isLoading || isChecking) {
     return (
       <div className="flex h-screen justify-center items-center">
         <Bars
