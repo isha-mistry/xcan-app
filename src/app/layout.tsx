68cc0489923fc4c66cc1079ecd6692f9fe967416
abhi152003 <abhiprajapati011@gmail.com>
2026-02-03 18:13:01 +0530

    added Open Graph image and configure Twitter card metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,24 +36,22 @@ export const metadata: Metadata = {
     description: "Learn. Experience. Build.",
     url: "https://xcan.dev/",
     siteName: "Xcan",
-
-    // images: [
-    //   {
-    //     url: "https://gateway.lighthouse.storage/ipfs/QmZmWxpdhQZnag8HZtwZPLR5wtK2jjfgsTBMpNpmijtZ5x",
-    //     width: 800,
-    //     height: 600,
-    //     alt: "img",
-    //   },
-    //   {
-    //     url: "https://gateway.lighthouse.storage/ipfs/QmZmWxpdhQZnag8HZtwZPLR5wtK2jjfgsTBMpNpmijtZ5x",
-    //     width: 1800,
-    //     height: 1600,
-    //     alt: "img",
-    //   },
-    // ],
-
+    images: [
+      {
+        url: "xcan-og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Xcan",
+      },
+    ],
     locale: "en_US",
     type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Xcan",
+    description: "Discover. Learn. Engage.",
+    images: ["xcan-og-image.png"],
   },
 
 };
